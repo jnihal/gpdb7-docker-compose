cf8ffb468eeba78bec4db40a4ebc88e7d1c4b00a
Nihal Jain <jnihal@vmware.com>
2024-02-13 00:32:51 +0530

    Add suport for both arm/amd based systems

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -3,8 +3,4 @@
 yum install -y epel-release
 yum install -y emacs-nox net-tools openssh-server openssh-clients passwd iproute procps vim go
 
-pip3 install psutil==5.7.0 pyyaml==5.3.1
-
-wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
-tar -C /usr/local -xzvf go1.18.3.linux-amd64.tar.gz
-rm -f go1.18.3.linux-amd64.tar.gz
+pip3 install psutil==5.7.0
